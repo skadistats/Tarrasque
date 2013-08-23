506770fb31d5c45db273f27594872cf26bc61161
Laurie Clark-Michalek <bluepeppers@archlinux.us>
2013-08-23 01:09:50 +0100

    Remove dynamic version stuff so we can use RTD

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,8 +18,6 @@ import sys, os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('../..'))
 
-import tarrasque
-
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
@@ -50,7 +48,7 @@ copyright = u'2013, Laurie Clark-Michalek'
 # built documents.
 #
 # The short X.Y version.
-version = tarrasque.__version__
+version = "0.1"
 # The full version, including alpha/beta/rc tags.
 release = version
 
