40f57016cb7c157f718d849cb5966ce8e496b57e
Laurie Clark-Michalek <bluepeppers@archlinux.us>
2013-09-10 00:08:34 +0100

    Added tests for remaining properties on player

--- tests/test_player.py
+++ tests/test_player.py
@@ -39,3 +39,43 @@ class PlayerTestCase(unittest.TestCase):
   def test_players_team(self):
     for player in self.replay.players:
       in_(player.team, ["radiant", "dire"])
+
+  def test_players_last_hits(self):
+    for player in self.replay.players:
+      gt_(player.last_hits, -1)
+
+  def test_players_denies(self):
+    for player in self.replay.players:
+      gt_(player.denies, -1)
+
+  def test_players_kills(self):
+    for player in self.replay.players:
+      gt_(player.kills, -1)
+
+  def test_players_deaths(self):
+    for player in self.replay.players:
+      gt_(player.deaths, -1)
+
+  def test_players_assists(self):
+    for player in self.replay.players:
+      gt_(player.assists, -1)
+
+  def test_players_streak(self):
+    for player in self.replay.players:
+      gt_(player.streak, -1)
+
+  def test_players_buyback_cooldown_time(self):
+    for player in self.replay.players:
+      gt_(player.buyback_cooldown_time, -1)
+
+  def test_players_last_buyback_time(self):
+    for player in self.replay.players:
+      gt_(player.last_buyback_time, -1)
+
+  def test_players_has_buyback(self):
+    for player in self.replay.players:
+      eq_(player.has_buyback, True)
+
+  def test_players_total_gold(self):
+    for player in self.replay.players:
+      eq_(player.total_gold, player.unreliable_gold + player.reliable_gold)
