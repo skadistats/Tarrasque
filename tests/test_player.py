d9665cf05e6a7ca738b960d47bef39a0a96a6464
Laurie Clark-Michalek <bluepeppers@archlinux.us>
2013-09-10 00:48:58 +0100

    Added tests for a specific player

--- tests/test_player.py
+++ tests/test_player.py
@@ -10,7 +10,8 @@ class PlayerTestCase(unittest.TestCase):
   @classmethod
   def setUpClass(cls):
     cls.replay = tarrasque.StreamBinding.from_file(cls.REPLAY_FILE,
-                                                   start_tick="game")
+                                                   start_tick=10000)
+    cls.player = cls.replay.players[0]
 
   def test_players_have_names(self):
     for player in self.replay.players:
@@ -79,3 +80,54 @@ class PlayerTestCase(unittest.TestCase):
   def test_players_total_gold(self):
     for player in self.replay.players:
       eq_(player.total_gold, player.unreliable_gold + player.reliable_gold)
+
+  def test_have_name(self):
+    eq_(self.player.name, "Gyozmo")
+
+  def test_player_have_hero(self):
+    neq_(self.player.hero, None)
+
+  def test_players_reliable_gold(self):
+    eq_(self.player.reliable_gold, 0)
+
+  def test_players_unreliable_gold(self):
+    eq_(self.player.unreliable_gold, 154)
+
+  def test_players_earned_gold(self):
+    eq_(self.player.earned_gold, 150)
+
+  def test_players_steam_id(self):
+    eq_(self.player.steam_id, 2865091096608769)
+
+  def test_players_team(self):
+    eq_(self.player.team, "radiant")
+
+  def test_players_last_hits(self):
+    eq_(self.player.last_hits, 1)
+
+  def test_players_denies(self):
+    eq_(self.player.denies, 0)
+
+  def test_players_kills(self):
+    eq_(self.player.kills, 0)
+
+  def test_players_deaths(self):
+    eq_(self.player.deaths, 0)
+
+  def test_players_assists(self):
+    eq_(self.player.assists, 0)
+
+  def test_players_streak(self):
+    eq_(self.player.streak, 0)
+
+  def test_players_buyback_cooldown_time(self):
+    eq_(self.player.buyback_cooldown_time, 0.0)
+
+  def test_players_last_buyback_time(self):
+    eq_(self.player.last_buyback_time, 0)
+
+  def test_players_has_buyback(self):
+    eq_(self.player.has_buyback, True)
+
+  def test_players_total_gold(self):
+    eq_(self.player.total_gold, 154)
