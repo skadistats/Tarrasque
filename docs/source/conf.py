012dba1ca2c39cffaac14a4a3068b4ef993d4a40
Laurie Clark-Michalek <bluepeppers@archlinux.us>
2013-09-12 13:23:35 +0100

    Mock skadi for autodoc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,6 +18,26 @@ import sys, os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('../..'))
 
+class Mock(object):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(cls, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            mockType = type(name, (), {})
+            mockType.__module__ = __name__
+            return mockType
+        else:
+            return Mock()
+
+sys.modules["skadi"] = Mock()
+
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
