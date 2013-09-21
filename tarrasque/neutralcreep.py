1233b02060ccaf5fd227fe9846c8785be38edeb1
thehellbean <andreasholmgren@hellbean.com>
2013-09-21 16:38:04 +0200

    Update neutralcreep.py

--- tarrasque/neutralcreep.py
+++ tarrasque/neutralcreep.py
@@ -3,11 +3,12 @@ from .lanecreep import LaneCreep
 from .entity import register_entity
 import re
 
+
+@register_entity('DT_DOTA_BaseNPC_Creep_Neutral')
+class NeutralCreep(LaneCreep):
 """
 A class for neutral creeps, does not seem to include Roshan (needs confirmation)
 """
-@register_entity('DT_DOTA_BaseNPC_Creep_Neutral')
-class NeutralCreep(LaneCreep):
     name = Property("DT_BaseEntity", "m_nModelIndex")\
         .apply(StringTableTrans("modelprecache"))\
         .apply(FuncTrans(lambda n: n[0]))\
