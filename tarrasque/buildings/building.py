e2ae961c8f290ba6ca6f567af952a4a2bc4947ef
Filip Johansson <filip@crazylogic.com>
2013-09-27 14:34:29 +0200

    Removed duplicate class definitions

--- tarrasque/buildings/building.py
+++ tarrasque/buildings/building.py
@@ -7,27 +7,3 @@ class Building(BaseNPC):
 
   Represents a building in the game.
   """
-
-@register_entity("DT_DOTA_BaseNPC_Tower")
-class Tower(Building):
-  """
-  Inherits from :class:`Building`.
-
-  Represents a Tower in the game.
-  """
-
-@register_entity("DT_DOTA_BaseNPC_Barracks")
-class Barrack(Building):
-  """
-  Inherits from :class:`Building`.
-
-  Represents a Barrack in the game.
-  """
-
-@register_entity("DT_DOTA_BaseNPC_Fort")
-class Ancient(Building):
-  """
-  Inherits from :class:`Building`.
-
-  Represents an Ancient in the game.
-  """
